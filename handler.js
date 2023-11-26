04246c4c565a365d1e5c4ae927fdc13400131747
vinwithin <kelvinadinata82@gmail.com>
2023-11-27 02:05:50 +0700

    first commit

---
+++ handler.js
@@ -0,0 +1,35 @@
+const TextToSpeech = require('@google-cloud/text-to-speech');
+require('dotenv').config();
+const fs = require('fs');
+const util = require('util');
+const client  = new TextToSpeech.TextToSpeechClient();
+const quickStart = async(request, h) => {
+    // The text to synthesize
+    const { text } = request.payload;
+  
+    // Construct the request
+    const process = {
+      input: {text: text},
+      // Select the language and SSML voice gender (optional)
+      voice: {languageCode: 'id-ID', ssmlGender: 'NEUTRAL'},
+      // select the type of audio encoding
+      audioConfig: {audioEncoding: 'MP3'},
+    };
+  
+    // Performs the text-to-speech request
+    const [output] = await client.synthesizeSpeech(process);
+    // Write the binary audio content to a local file
+    const writeFile = util.promisify(fs.writeFile);
+    // const id = nanoid(16);
+    const success = await writeFile("output.mp3", output.audioContent, 'binary');
+    if(writeFile){
+      const respone = h.response({
+        status : 'success',
+        message: 'berhasil menambahkan suara',
+      }).code(201);
+      return respone;
+    }
+    
+    
+  }
+ module.exports = quickStart;
---
+++ server.js
@@ -0,0 +1,34 @@
+const Hapi = require('@hapi/hapi');
+const quickStart = require('./handler');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    });
+    server.route([
+        {
+            method: "GET",
+            path: '/',
+            handler: (request, h) => {
+                return 'hello world'
+            }
+        },
+        {
+            method: "POST",
+            path: '/text-to-speech',
+            handler: quickStart,
+        }
+    ])
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
